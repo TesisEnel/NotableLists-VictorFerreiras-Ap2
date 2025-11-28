3d36e6548d665c723bf82c0d804e241f765e199c
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-11-28 16:33:40 -0600

    Creado GetAllUsersUseCase.kt

---
+++ app/src/main/java/ucne/edu/notablelists/domain/friends/usecase/GetAllUsersUseCase.kt
@@ -0,0 +1,12 @@
+package ucne.edu.notablelists.domain.friends.usecase
+
+import ucne.edu.notablelists.data.remote.Resource
+import ucne.edu.notablelists.data.remote.dto.UserResponseDto
+import ucne.edu.notablelists.domain.friends.FriendsRepository
+import javax.inject.Inject
+
+class GetAllUsersUseCase @Inject constructor(
+    private val repository: FriendsRepository
+) {
+    suspend operator fun invoke(): Resource<List<UserResponseDto>> = repository.getAllUsers()
+}
