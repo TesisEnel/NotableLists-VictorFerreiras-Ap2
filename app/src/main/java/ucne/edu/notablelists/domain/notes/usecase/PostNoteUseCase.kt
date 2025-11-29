4b070867da146b7e745e269fea81b0759eedd839
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-11-28 20:28:26 -0600

    Cambiado a resource

--- app/src/main/java/ucne/edu/notablelists/domain/notes/usecase/PostNoteUseCase.kt
+++ app/src/main/java/ucne/edu/notablelists/domain/notes/usecase/PostNoteUseCase.kt
@@ -1,5 +1,6 @@
 package ucne.edu.notablelists.domain.notes.usecase
 
+import ucne.edu.notablelists.data.remote.Resource
 import ucne.edu.notablelists.domain.notes.model.Note
 import ucne.edu.notablelists.domain.notes.repository.NoteRepository
 import javax.inject.Inject
@@ -7,5 +8,5 @@ import javax.inject.Inject
 class PostNoteUseCase @Inject constructor(
     private val repository: NoteRepository
 ) {
-    suspend operator fun invoke(note: Note): Note = repository.postNote(note)
+    suspend operator fun invoke(note: Note): Resource<Note> = repository.postNote(note)
 }
