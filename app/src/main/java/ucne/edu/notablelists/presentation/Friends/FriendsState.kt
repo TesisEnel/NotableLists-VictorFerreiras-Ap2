51e5d0cb99e39c5d158b4a83c5b9b82ccdd24dc0
VSFerreiras <vsferreiras402@protonmail.com>
2025-12-01 00:02:43 -0400

    Feat: Creado state de friends

---
+++ app/src/main/java/ucne/edu/notablelists/presentation/Friends/FriendsState.kt
@@ -0,0 +1,17 @@
+package ucne.edu.notablelists.presentation.friends
+
+import ucne.edu.notablelists.domain.friends.model.Friend
+import ucne.edu.notablelists.domain.friends.model.PendingRequest
+import ucne.edu.notablelists.domain.users.model.User
+
+data class FriendsState(
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val friends: List<Friend> = emptyList(),
+    val pendingRequests: List<PendingRequest> = emptyList(),
+    val searchResults: List<User> = emptyList(),
+    val searchQuery: String = "",
+    val selectedTabIndex: Int = 0,
+    val errorMessage: String? = null,
+    val successMessage: String? = null
+)
