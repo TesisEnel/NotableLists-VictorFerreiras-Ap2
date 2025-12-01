58c349c24c13b3fce9501155f670149b596d32a0
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-11-30 20:39:26 -0600

    refactor: implements new interface

--- app/src/main/java/ucne/edu/notablelists/data/local/AlarmScheduler.kt
+++ app/src/main/java/ucne/edu/notablelists/data/local/AlarmScheduler.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import dagger.hilt.android.qualifiers.ApplicationContext
+import ucne.edu.notablelists.domain.notification.ReminderScheduler
 import ucne.edu.notablelists.presentation.notification.ReminderReceiver
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -13,10 +14,10 @@ import javax.inject.Inject
 
 class AlarmScheduler @Inject constructor(
     @ApplicationContext private val context: Context
-) {
+) : ReminderScheduler {
     private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-    fun schedule(noteId: String, title: String, time: LocalDateTime) {
+    override fun schedule(noteId: String, title: String, time: LocalDateTime) {
         val intent = Intent(context, ReminderReceiver::class.java).apply {
             putExtra("NOTE_ID", noteId)
             putExtra("NOTE_TITLE", title)
@@ -55,7 +56,7 @@ class AlarmScheduler @Inject constructor(
         }
     }
 
-    fun cancel(noteId: String) {
+    override fun cancel(noteId: String) {
         val intent = Intent(context, ReminderReceiver::class.java)
         val pendingIntent = PendingIntent.getBroadcast(
             context,
