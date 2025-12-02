ccdb715e9b8d0aff1b83a2da3c06a834cd26a907
VSFerreiras <vsferreiras402@protonmail.com>
2025-12-01 21:56:43 -0400

    Refactor: Corregido uso de currentuser en el user state

--- app/src/main/java/ucne/edu/notablelists/presentation/users/UserState.kt
+++ app/src/main/java/ucne/edu/notablelists/presentation/users/UserState.kt
@@ -1,11 +1,18 @@
 package ucne.edu.notablelists.presentation.users
 
+import ucne.edu.notablelists.domain.users.model.User
+
 data class UserState(
+    val users: List<User> = emptyList(),
     val username: String = "",
     val password: String = "",
+    val currentUser: String = "",
+    val currentUserId: Int? = null,
     val isLoading: Boolean = false,
     val error: String? = null,
+    val isSuccess: Boolean = false,
     val usernameError: String? = null,
     val passwordError: String? = null,
-    val showSkipDialog: Boolean = false
+    val showSkipDialog: Boolean = false,
+    val isSessionChecked: Boolean = false
 )
